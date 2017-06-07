e55567f546ce0c1fe6e7df584349928f643ff61b
Ederik van Koldenhoven <e.vankoldenhoven@gmail.com>
2017-06-07 09:06:21 +0200

    finished rails wikikimous

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,33 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new
+    @article.save
   end
 
   def edit
+    @article = Article.find(params[:id])
+
   end
 
   def update
+    @article = Article.find(params[:id])
+    @article.update
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
   end
 end
